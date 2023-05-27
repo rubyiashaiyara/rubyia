c609dcf116c3334d11248f024da19a32799496c3
rubyiashaiyara <134300469+rubyiashaiyara@users.noreply.github.com>
2023-05-27 22:44:08 +0600

    Add files via upload

---
+++ good.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <script>
+      var num = prompt("enter num");
+
+      if (num==100) {
+        console.log("Excellent");
+      } else if(num==70) 
+      {
+        console.log("Good");
+      }
+      else
+      {
+        console.log('Need Improvement');
+      }
+    </script>
+  </body>
+</html>
+Write to Samiul Alom
